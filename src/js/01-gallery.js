1aa091524103f2e62b9154b48411b9088d42246b
Yehor Kikas <ekikas13@gmail.com>
2022-09-29 13:17:14 +0300

    add simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+const galleryList = document.querySelector('.gallery');
+const addImagesMarkup = galleryItems
+   .map(
+      img =>
+         `<a class="gallery__item" href="${img.original}">
+   <img
+     class="gallery__image"
+     src="${img.preview}"
+     data-source="${img.original}"
+     alt="${img.description}"
+     title = "${img.description}"
+   />
+ </a></li>`
+   )
+   .join('');
+galleryList.insertAdjacentHTML('beforeend', addImagesMarkup);
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
 console.log(galleryItems);
