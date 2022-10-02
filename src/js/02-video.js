6ee010abf9da143638e86857a6a346131066fdc8
Yehor Kikas <ekikas13@gmail.com>
2022-10-02 21:06:51 +0300

    fix 3 exrcise

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,5 +7,13 @@ player.on('timeupdate', throttle((data) => {
    localStorage.setItem("videoplayer-current-time", data.seconds)
 }, 1000));
 player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function (seconds) {
-});
+})
+   .catch(function (error) {
+      switch (error.name) {
+         case 'RangeError':
+            break;
+         default:
+            break;
+      }
+   });
 
